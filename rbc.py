2b4c5a949c23b3bb3a3cea6a89ea15b1b5b65024
Easy Rider <115343312+mironovms@users.noreply.github.com>
2023-04-26 19:21:16 +0300

    1.1 parsing JSON

--- rbc.py
+++ rbc.py
@@ -1,3 +1,10 @@
 import requests
-import json # импортируем необходимую библиотеку
+import json
 
+
+date = '2022-01-21'
+key = 'DEMO_KEY'
+r = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={date}&api_key={key}")
+data = json.loads(r.content)
+id_2_photo = data['photos'][1]['id']
+print(id_2_photo)
